b9c661229b21877c7f8b57be914b6634bae76843
geon-youn <geon.youn@outlook.com>
2023-08-26 11:34:58 -0400

    create models

---
+++ models/message.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Message = new Schema({
+  title: { type: String, required: true },
+  timestamp: { type: Date, required: true },
+  text: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, required: true },
+});
+
+module.exports = mongoose.model('Message', Message);
---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+User.virtual('fullname').get(function () {
+  return this.fname + ' ' + this.lname;
+});
+
+module.exports = mongoose.model('User', User);
