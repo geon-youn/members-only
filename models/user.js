be51d7d20419bae4aab66b20ab132cd767d188cf
geon-youn <geon.youn@outlook.com>
2023-08-26 15:41:59 -0400

    add optional member and admin booleans to user

--- models/user.js
+++ models/user.js
@@ -6,7 +6,8 @@ const User = new Schema({
   lname: { type: String, required: true },
   username: { type: String, required: true },
   password: { type: String, required: true },
-  member: { type: Boolean, required: true },
+  member: { type: Boolean },
+  admin: { type: Boolean },
 });
 
 User.virtual('fullname').get(function () {
