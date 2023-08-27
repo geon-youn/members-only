34e472911b5decbd242b3ab29576f44e9072a3fb
geon-youn <geon.youn@outlook.com>
2023-08-27 10:00:47 -0400

    add reference to user

--- models/message.js
+++ models/message.js
@@ -5,7 +5,7 @@ const Message = new Schema({
   title: { type: String, required: true },
   timestamp: { type: Date, required: true },
   text: { type: String, required: true },
-  author: { type: Schema.Types.ObjectId, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
 module.exports = mongoose.model('Message', Message);
